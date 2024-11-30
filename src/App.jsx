499c63471779d51e529eed303ed327925d58606d
Yanis <skyhard94@gmail.com>
2024-11-30 10:48:25 +0100

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Hero from "./components/Hero.jsx";
+
+const App = () => {
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero />
+
+      <section className="z-0 min-h-screen bg-blue-500" />
+    </main>
+  );
+};
+export default App;
