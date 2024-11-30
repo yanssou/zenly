ec7c213ac71161685972b9fc9a577113c7f58f14
Yanis <skyhard94@gmail.com>
2024-11-30 11:01:23 +0100

    Added about section with full size image scrolling animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
+import About from "./components/About.jsx";
 import Hero from "./components/Hero.jsx";
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero />
-
-      <section className="z-0 min-h-screen bg-blue-500" />
+      <About />
     </main>
   );
 };
---
+++ src/components/AnimatedTitle.jsx
@@ -0,0 +1,10 @@
+const AnimatedTitle = () => {
+  return (
+    <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
+      Déc<b>o</b>uvrez la plus grande avanture collaborative <br />
+      de <b>t</b>ous les temps
+    </div>
+  );
+};
+
+export default AnimatedTitle;
