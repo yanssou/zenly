0a78023d5769bbe1bb621a900e1bb5ac6ebc857b
Yanis <skyhard94@gmail.com>
2024-12-01 11:00:32 +0100

    features page start

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import About from "./components/About.jsx";
+import Features from "./components/Features.jsx";
 import Hero from "./components/Hero.jsx";
 import Navbar from "./components/Navbar.jsx";
 
@@ -8,6 +9,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <About />
+      <Features />
     </main>
   );
 };
---
+++ src/components/Features.jsx
@@ -0,0 +1,20 @@
+const Features = () => {
+  return (
+    <section className="bg-black pb-52">
+      <div className="container mx-auto px-3 md:px-10">
+        <div className="px-5 py-32">
+          <p className="font-circular-web text-lg text-blue-50">
+            Plongez dans le metagame
+          </p>
+        </div>
+        <p>
+          Plongez dans un univers riche et en constante expansion, où une
+          multitude de produits dynamiques se rejoignent pour créer une
+          expérience superposée et interconnectée à votre monde
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Features;
