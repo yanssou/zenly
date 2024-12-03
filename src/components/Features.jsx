270b67586afb2b4b612b5a59522c90e3ab9d5455
Yanis <skyhard94@gmail.com>
2024-12-03 23:26:11 +0100

    Added animation, bento cards

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,3 +1,68 @@
+/* eslint-disable react/prop-types */
+import { useRef, useState } from "react";
+import { TiLocationArrow } from "react-icons/ti";
+
+const BentoTilt = ({ children, className = "" }) => {
+  const [transformStyle, setTransformStyle] = useState("");
+  const itemRef = useRef();
+
+  const handleMouseMove = (e) => {
+    if (!itemRef.current) return;
+
+    const { left, top, width, height } =
+      itemRef.current.getBoundingClientRect();
+
+    const relativeX = (e.clientX - left) / width;
+    const relativeY = (e.clientY - top) / height;
+
+    const tiltX = (relativeY - 0.5) * 10;
+    const tiltY = (relativeX - 0.5) * -10;
+
+    const newTransform = `perspective(700px) rotateX(${tiltX}deg) rotateY(${tiltY}deg) scale3d(0.95, 0.95, 0.95)`;
+
+    setTransformStyle(newTransform);
+  };
+
+  const handleMouseLeave = () => {
+    setTransformStyle("");
+  };
+
+  return (
+    <div
+      className={className}
+      ref={itemRef}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+      style={{ transform: transformStyle }}
+    >
+      {children}
+    </div>
+  );
+};
+
+// on crée le composant ici car c'est le seul endroit où on l'utilisera
+const BentoCard = ({ src, title, description }) => {
+  return (
+    <div className="relative size-full">
+      <video
+        src={src}
+        loop
+        muted
+        autoPlay
+        className="absolute left-0 top-0 size-full object-cover object-center"
+      />
+      <div className="relative z-10 flex size-full flex-col justify-between p-5 text-blue-50">
+        <div>
+          <h1 className="bento-title special-font">{title}</h1>
+          {description && (
+            <p className="mt-3 max-w-64 text-xs md:text-base">{description}</p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const Features = () => {
   return (
     <section className="bg-black pb-52">
@@ -6,12 +71,82 @@ const Features = () => {
           <p className="font-circular-web text-lg text-blue-50">
             Plongez dans le metagame
           </p>
+          <p className="max-w-md font-circular-web text-lg text-blue-50 opacity-50">
+            Plongez dans un univers riche et en constante expansion, où une
+            multitude de produits dynamiques se rejoignent pour créer une
+            expérience superposée et interconnectée à votre monde
+          </p>
         </div>
-        <p>
-          Plongez dans un univers riche et en constante expansion, où une
-          multitude de produits dynamiques se rejoignent pour créer une
-          expérience superposée et interconnectée à votre monde
-        </p>
+
+        <BentoTilt className="border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
+          <BentoCard
+            src="videos/feature-1.mp4"
+            title={
+              <>
+                radia<b>n</b>t
+              </>
+            }
+            description="Un jeu crossplatform qui transforme vos activités dans les jeux Web2 et Web3 en une avanture gratifiante."
+          />
+        </BentoTilt>
+
+        <div className="grid h-[135vh] grid-cols-2 grid-rows-3 gap-7">
+          <BentoTilt className="bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2">
+            <BentoCard
+              src="videos/feature-2.mp4"
+              title={
+                <>
+                  Zig<b>m</b>a
+                </>
+              }
+              description="Une collection NFT inspirée des animes et des jeux vidéos."
+            />
+          </BentoTilt>
+
+          <BentoTilt className="bento-tilt_1 row-span-1 ms-32 md:col-span-1 md:ms-0">
+            <BentoCard
+              src="videos/feature-3.mp4"
+              title={
+                <>
+                  N<b>e</b>xus
+                </>
+              }
+              description="Un hub social gamifié qui ajoute une dimension ludique à vos interactions sociales dans les communautés Web3."
+            />
+          </BentoTilt>
+
+          <BentoTilt className="bento-tilt_1 me-14 md:col-span-1 md:me-0">
+            <BentoCard
+              src="videos/feature-4.mp4"
+              title={
+                <>
+                  Az<b>u</b>l
+                </>
+              }
+              description="Un agent IA inter-mondes – qui élève votre expérience de jeu pour la rendre plus amusante et productive."
+            />
+          </BentoTilt>
+
+          <div className="bento-tilt_2">
+            <div className="flex size-full flex-col justify-between bg-violet-300 p-5">
+              <h1 className="bento-title special-font max-w-64 text-black">
+                Enc<b>o</b>re plus a ve<b>n</b>ir bi<b>e</b>ntot !
+              </h1>
+
+              <TiLocationArrow className="m-5 scale-[5] self-end" />
+            </div>
+          </div>
+
+          <BentoTilt className="bento-tilt_2">
+            <video
+              src="videos/feature-5.mp4"
+              loop
+              autoPlay
+              muted
+              className="size-full object-cover object-center"
+            />
+          </BentoTilt>
+        </div>
       </div>
     </section>
   );
